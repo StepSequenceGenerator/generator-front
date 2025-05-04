d64f646d673b4102ec43dc95fb24c4c73ebaefb1
gen <kyliki@yandex.ru>
2025-05-04 21:43:22 +0300

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'sg-front',
-      script: 'node_modules/next/dist/bin/next',
+      script: './.next/standalone/server.js',
       args: 'start',
       cwd: '/home/genry_bolit/sg-front/current',
       env: {
+        NODE_ENV: 'production',
         PORT: 4500 // Дублируем для надежности
       }
     }
