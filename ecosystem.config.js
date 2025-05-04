4fa20714150a1bee2afe59f44715e55f2f541959
gen <kyliki@yandex.ru>
2025-05-04 23:12:22 +0300

    update ecosystem, next.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,20 @@ module.exports = {
       name: 'sg-front',
       script: './.next/standalone/server.js',
       args: 'start',
-      cwd: '/home/genry_bolit/sg-front/current',
+      cwd: __dirname,
       env: {
         NODE_ENV: 'production',
         PORT: 4500 // Дублируем для надежности
-      }
+      },
+      watch: false,
+      autorestart: true,
+      restart_delay: 1000,
+      max_restarts: 10,
+      instance_var: 'INSTANCE_ID',
+      error_file: './logs/error.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true
     }
   ]
 }
